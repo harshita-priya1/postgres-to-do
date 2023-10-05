e65b9aa590a9f60b5bb78a27c89f30178b0e0bc1
HARSHITA PRIYA <harshitapriyaph5@gmail.com>
2023-10-05 20:19:19 +0100

    sorting map listitem

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
 import ListHeader from "./components/ListHeader";
+import ListItem from "./components/ListItem";
 
 const App = () => {
+  const userEmail = "harshita@test.com";
+  const [tasks, setTasks] = useState(null);
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
+      const jsonData = await response.json();
+      setTasks(jsonData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(tasks);
+  //sort by date
+  const sortedTasks = tasks?.sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+
   return (
     <div className="app">
       <ListHeader listName={"🏔️ Holiday Tick List"} />
+      {sortedTasks?.map((task) => (
+        <ListItem key={task.id} task={task} />
+      ))}
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 const PORT = process.env.PORT || 8000;
 const express = require("express");
+const cors = require("cors");
 
 const pool = require("./db");
 
 const app = express();
 
+app.use(cors());
+
 //get all todos
-app.get("/todos", async (req, res) => {
+app.get("/todos/:userEmail", async (req, res) => {
+  console.log(req);
+  const { userEmail } = req.params;
+  console.log(userEmail);
   try {
-    const todos = await pool.query("SELECT * FROM todos");
+    const todos = await pool.query("SELECT * FROM todos WHERE user_email= $1", [
+      userEmail,
+    ]);
     res.json(todos.rows);
   } catch (error) {
     console.log(error);
